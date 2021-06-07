5cc3ee5137d1872ff38c4ec94f126f0b22bc6d3c
djso89 <danielso89@gmail.com>
2021-06-07 10:11:23 -0700

    hodor: level 2

--- level_2/main.py
+++ level_2/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-hodor challenge level 1
+hodor challenge level 2
 """
 
 
@@ -9,7 +9,13 @@ import requests
 
 
 page = 'http://158.69.76.135/level2.php'
+user_agent = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:64.0) "
+              "Gecko/20100101 Firefox/64.0")
 
+header = {
+    "User-Agent": user_agent,
+    "referer": page
+}
 
 vote = {
     "id":"1448",
@@ -18,7 +24,7 @@ vote = {
 }
 
 if __name__ == "__main__":
-    vote_times = 4096 - 1216
+    vote_times = 1024
     for i in range(0, vote_times):
         print('vote #: ' + str(i + 1))
         print('processing...........')
